6bd785e42a33f46f4e7bc79a089c734829ea4f6f
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-10 04:44:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  alans_new_info = "GUI"
+    alans_new_info = "GUI"
     programmer_hash[:alan_kay][:known_for] = alans_new_info
+    programmer_hash
 end
 
 def adding_to_dennis
