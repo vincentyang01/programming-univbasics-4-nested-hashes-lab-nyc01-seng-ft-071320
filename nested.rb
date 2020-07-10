e3f00f21774fb2aa9a8fbe16fef922f5828e5b6c
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-10 04:43:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,6 @@ def adding_matz
     alans_new_info = "GUI"
     programmer_hash[:alan_kay][:known_for] = alans_new_info
     
-
-
-programmer_hash.merge(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 end
 
 def changing_alan
